40bbf6dd0be1cc0dd085e268ab668013ce2e13cf
Denis <64834314+sanddwich@users.noreply.github.com>
2023-10-10 18:25:29 +0400

    213123

--- src/main/java/spring/auth/entities/AbstractEntity.java
+++ src/main/java/spring/auth/entities/AbstractEntity.java
@@ -1,7 +1,7 @@
 package spring.auth.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,6 +10,9 @@ import java.util.Date;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class AbstractEntity {
 
     @Id
--- src/main/java/spring/auth/rest/controller/users/RestApiUserController.java
+++ src/main/java/spring/auth/rest/controller/users/RestApiUserController.java
@@ -5,19 +5,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring.auth.entities.User;
+import spring.auth.services.UserService;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class RestApiUserController {
+    private final UserService userService;
+
     @GetMapping({"/", ""})
     public ResponseEntity index() {
         Map<String, Object> result = new HashMap<>();
         result.put("message", "/api/v1/users Controller");
 
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping({"/get_all", "/get_all/"})
+    public ResponseEntity getAll() {
+        Map<String, List<User>> response = new HashMap<>();
+        response.put("users", this.userService.findAll());
+
+        return ResponseEntity.ok(response);
     }
 }
