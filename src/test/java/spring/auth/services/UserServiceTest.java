8cbae837da988c5c24d431f383eb7c295fa02352
Denis <64834314+sanddwich@users.noreply.github.com>
2023-11-07 23:17:05 +0400

    213132

---
+++ src/test/java/spring/auth/services/UserServiceTest.java
@@ -0,0 +1,71 @@
+package spring.auth.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.reset;
+
+class UserServiceTest {
+
+    @AfterEach
+    void tearDown() {
+        reset();
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findByUsernameOREmail() {
+    }
+
+    @Test
+    void findByAccessToken() {
+    }
+
+    @Test
+    void userAlreadyExist() {
+    }
+
+    @Test
+    void clearAccessToken() {
+    }
+
+    @Test
+    void findByUsername() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void findBySurname() {
+    }
+}
